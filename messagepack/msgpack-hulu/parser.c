393ac93060e2d281580eb1fffcd50d18230c6f07
root <root@precise64.(none)>
2016-03-11 15:20:28 +0000

    update

--- messagepack/msgpack-hulu/parser.c
+++ messagepack/msgpack-hulu/parser.c
@@ -27,7 +27,7 @@ char *parseString(_byte *buf, int *off){
     }
     
     switch(head & 0xFF){
-        case  0xD9: 
+        case 0xD9: 
             len = *(buf+1);
             printf("len: %d\n", len);
             str = (char *) malloc(sizeof(char)*len);
@@ -54,13 +54,52 @@ char *parseString(_byte *buf, int *off){
 }
 
 int parseBool(_byte *buf, int *off){
+    _byte head = *buf;
     *off +=  1;
-    if(*buf == 0xC2)
+    if(head == 0xC2)
         return 0;
-    if(*buf == 0xC3)
+    if(head == 0xC3)
         return 1;
+
+    return -1; 
 }
 
+void *parseNil(_byte *buf, int *off){
+    _byte head = *buf;
+    *off += 1;
+    if(head == 0xC0)
+        return NULL;
+    return NULL;
+}
+
+int parseInt(_byte *buf, int *off){
+    char head = *buf;
+    int len = 0;
+    
+    if( (head & 0x80) == 0){
+       int val = (*buf) & 0x7F; 
+       *off += 1;
+       return val;
+    }
+
+    if( (head & 0xE0) == 0xE0){
+        int val = (*buf) & 0x1F;
+        *off += 1;
+        return -val;
+    }
+
+    switch(head){
+        case(0xCC):
+            len = *(buf_+ 1);
+        case(0xCD):
+        case(0xCE):
+        case(0xCF):
+        case(0xD0):
+        case(0xD1):
+        case(0xD2):
+        case(0xD3):
+    }
+}
 
 int main(){
     _byte ext[] = {0xC7, 0x11, 0x00, 0xA4, 0x6E, 0x61, 0x6D, 0x65, 0xA6, 0xE5, 0x91, 0xB5, 0xE5, 0x91, 0xB5, 0xA3, 0x61, 0x67, 0x65, 0x0A};
