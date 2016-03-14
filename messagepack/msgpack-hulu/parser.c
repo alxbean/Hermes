4a5631779fb7adf3856c1cac4de937f2d0953454
alxbean <alxbeang@gmail.com>
2016-03-14 10:24:28 +0000

    add float

--- messagepack/msgpack-hulu/parser.c
+++ messagepack/msgpack-hulu/parser.c
@@ -9,11 +9,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-//#include "parse_msg.h"
-#define _byte unsigned char
+typedef unsigned char ubyte_t ;
 
-char *parseString(_byte *buf, int *off){
-    _byte head = buf[0];        
+char *parseString(ubyte_t *buf, int *off){
+    ubyte_t head = *buf & 0xFF;
     char *str = NULL;
     int len = 0;
 
@@ -22,59 +21,64 @@ char *parseString(_byte *buf, int *off){
         printf("len: %d\n", len);
         str = (char *) malloc(sizeof(char)*len);
         memcpy(str, (buf + 1), len);
-        off += len + 1;
+        *off += (len + 1);
         return str;
     }
     
-    switch(head & 0xFF){
+    switch(head){
         case 0xD9: 
             len = *(buf+1);
             printf("len: %d\n", len);
             str = (char *) malloc(sizeof(char)*len);
             memcpy(str, (buf + 2), len);
-            off += len + 1;
+            *off += (len + 1);
             return str;
         case 0xDA:
             len = (*(buf+1)<<8) + *(buf+2);
             printf("len: %d\n", len);
             str = (char *) malloc(sizeof(char)*len);
             memcpy(str, (buf+3), len);
-            off += len + 1;
+            *off += (len + 1);
             return str;
         case 0xDB:
             len = (*(buf + 1)<<24) + (*(buf + 2)<<16) + (*(buf + 3)<<8) + *(buf + 4);
             printf("len: %d\n", len);
             str = (char *) malloc(sizeof(char)*len);
             memcpy(str, (buf+5), len);
-            off += len+ 1;
+            *off += (len+ 1);
             return str;
     }
 
     return str;
 }
 
-int parseBool(_byte *buf, int *off){
-    _byte head = *buf;
+int parseBool(ubyte_t *buf, int *off){
+    ubyte_t head = *buf;
     *off +=  1;
     if(head == 0xC2)
         return 0;
-    if(head == 0xC3)
+    else if(head == 0xC3)
         return 1;
-
-    return -1; 
+    else
+        return -1;
 }
 
-void *parseNil(_byte *buf, int *off){
-    _byte head = *buf;
+void *parseNil(ubyte_t *buf, int *off){
+    ubyte_t head = *buf;
     *off += 1;
     if(head == 0xC0)
         return NULL;
     return NULL;
 }
 
-int parseInt(_byte *buf, int *off){
-    char head = *buf;
-    int len = 0;
+int parseInt(ubyte_t *buf, int *off){/*{{{*/
+    ubyte_t head = *buf & 0xFF;
+    unsigned int uval_8 = 0, uval_16 = 0, uval_32 = 0;
+    unsigned long long uval_64 = 0;
+    signed int val_8 = 0, val_16 = 0, val_32 = 0;
+    long long val_64 = 0;
+    unsigned long long tmp = 0;
+    int i = 0;
     
     if( (head & 0x80) == 0){
        int val = (*buf) & 0x7F; 
@@ -89,24 +93,104 @@ int parseInt(_byte *buf, int *off){
     }
 
     switch(head){
-        case(0xCC):
-            len = *(buf_+ 1);
-        case(0xCD):
-        case(0xCE):
-        case(0xCF):
-        case(0xD0):
-        case(0xD1):
-        case(0xD2):
-        case(0xD3):
+        case 0xCC:
+            uval_8 = (unsigned int) *(buf + 1);
+            printf("val: %u\n", uval_8);
+            *off += 2;
+            return uval_8;
+            
+        case 0xCD:
+            uval_16 = (unsigned int)(*(buf + 1) << 8) + *(buf + 2);
+            printf("val: %u\n", uval_16);
+            *off += 3;
+            return uval_16;
+            
+        case 0xCE:
+            uval_32 = (unsigned int)((*(buf + 1) << 24) + (*(buf + 2) << 16) + (*(buf + 3) << 8) + *(buf + 4));
+            printf("val: %u\n", uval_32);
+            *off += 5;
+            return uval_32;
+
+        case 0xCF:
+            tmp = *(buf + 1);
+            for(i = 0; i < 8; i++){
+                tmp = *(buf + 8 - i );
+                uval_64 += tmp << (i * 8);
+            }
+            printf("val: %llu\n", uval_64);
+            *off += 9;
+            return uval_64;
+
+        case 0xD0:
+            val_8 = (int) *(buf + 1);
+            printf("val: %d\n", uval_8);
+            *off += 2;
+            return uval_8;
+
+        case 0xD1:
+            val_16 = (int) ((*(buf + 1) << 8) + *(buf + 2));
+            printf("val: %d\n", val_16);
+            *off += 3;
+            return val_16;
+
+        case 0xD2:
+            val_32 = (int) ((*(buf + 1) << 24) + (*(buf + 2) << 16) + (*(buf + 3) << 8) + *(buf + 4));
+            printf("val: %u\n", val_32);
+            *off += 5;
+            return val_32;
+
+        case 0xD3:
+            tmp = *(buf + 1);
+            for(i = 0; i < 8; i++){
+                tmp = *(buf + 8 - i );
+                val_64 += tmp << (i * 8);
+            }
+            printf("val: %llu\n", val_64);
+            *off += 9;
+            return val_64;
     }
+
+    return 0;
+}/*}}}*/
+
+float parseFloat(ubyte_t *buf, int *off){
+    ubyte_t head = *buf;
+    union{
+        float float_tmp;
+        ubyte_t float_buf[4]; 
+        double double_tmp;
+        ubyte_t double_buf[8];
+    } float_union;
+
+    if(head == 0xCA){
+        int i = 0;
+        for(i = 0; i < 4; i++){
+            float_union.float_buf[i] = *(buf + 4 - i);  
+        }
+        *off += 5;
+        return float_union.float_tmp;
+    }
+    if(head == 0xCB){
+        int i  = 0;
+        for(i = 1; i<=8; i++){
+            float_union.double_buf[i] = *(buf + 8 - i);
+        }
+        *off += 9;
+        return float_union.double_tmp;
+    }
+
+    return 0.0; 
 }
 
 int main(){
-    _byte ext[] = {0xC7, 0x11, 0x00, 0xA4, 0x6E, 0x61, 0x6D, 0x65, 0xA6, 0xE5, 0x91, 0xB5, 0xE5, 0x91, 0xB5, 0xA3, 0x61, 0x67, 0x65, 0x0A};
-    _byte str[] = {0xD9, 0x29, 0x61, 0x62, 0x63, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6D, 0x6D, 0x6D, 0x6D, 0x6D, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73,  0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73};
+    ubyte_t ext[] = {0xC7, 0x11, 0x00, 0xA4, 0x6E, 0x61, 0x6D, 0x65, 0xA6, 0xE5, 0x91, 0xB5, 0xE5, 0x91, 0xB5, 0xA3, 0x61, 0x67, 0x65, 0x0A};
+    ubyte_t str[] = {0xD9, 0x29, 0x61, 0x62, 0x63, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6D, 0x6D, 0x6D, 0x6D, 0x6D, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73,  0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73};
     
     int off = 0;
-    char *ret_str = parseString(str, &off);
-    printf("%s\n", ret_str);
+    //char *ret_str = parseString(str, &off);
+    //printf("%s\n", ret_str);
+    
+    ubyte_t float_val[] = {0xCA, 0x43, 0x5C, 0x00, 0x00};
+    printf("%f\n", parseFloat(float_val, &off));
 }
 
---
+++ messagepack/msgpack-hulu/test.c
@@ -0,0 +1,35 @@
+/*************************************************************************
+    > File Name: test.c
+    > Author: shuaixiang
+    > Mail: shuaixiang@yuewen.com
+    > Created Time: Mon 14 Mar 2016 08:37:23 AM UTC
+ ************************************************************************/
+
+#include <stdio.h>
+
+typedef unsigned char uint8_t;
+
+typedef struct {
+    uint8_t size;
+    const char *ptr;
+} msgpack_object_str;
+
+
+typedef union{
+    int a;
+    msgpack_object_str str; 
+} msgpack_object_union;
+
+typedef struct {
+    uint8_t TYPE;
+    msgpack_object_union via;
+} msgpack_object;
+
+
+int main(){
+    uint8_t test[] = {0xAA, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x77, 0x6F, 0x72, 0x6C, 0x64};
+
+    msgpack_object *obj = (msgpack_object *) test;
+
+    printf("%s\n", obj->via.str.ptr);
+}
