870af40bce280c8aec02952bc949feb7d58c67cc
root <root@precise64.(none)>
2016-03-20 15:26:39 +0000

    update

--- messagepack/msgpack-hulu/include/msg_define.h
+++
@@ -1,8 +0,0 @@
-/*************************************************************************
-    > File Name: msgdefine.h
-    > Author: shuaixiang
-    > Mail: shuaixiang@yuewen.com
-    > Created Time: Mon 07 Mar 2016 08:05:25 AM UTC
- ************************************************************************/
-
-#define FIXINT
