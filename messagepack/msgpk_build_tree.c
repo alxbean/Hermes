e6368ed691e5c90fcccc59918197a2ad06124823
alxbean <alxbeang@gmail.com>
2016-12-17 15:06:43 +0800

    fix a bug

--- messagepack/msgpk_build_tree.c
+++ messagepack/msgpk_build_tree.c
@@ -1507,11 +1507,12 @@ static int bfs_queue_push(struct msgpk_object* obj){/*{{{*/
 }/*}}}*/
 
 static struct msgpk_object* bfs_queue_pop(){/*{{{*/
+    pthread_mutex_lock(&bq.mutex);
     if (NULL == bq.head){
         printf("queue is empty\n");
+        pthread_mutex_unlock(&bq.mutex);
         return NULL;
     } else {
-        pthread_mutex_lock(&bq.mutex);
         struct bfs_queue_node* node = bq.head;
         bq.head = bq.head->next_node;
         if (NULL == bq.head)
